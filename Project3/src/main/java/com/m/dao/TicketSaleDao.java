75fadcfb86717e6aa223e2fa5666011eb9ef505e
Mary <uniwanling@gmail.com>
2020-01-07 18:32:25 +0800

    no message

--- Project3/src/main/java/com/m/dao/TicketSaleDao.java
+++ Project3/src/main/java/com/m/dao/TicketSaleDao.java
@@ -1,18 +1,25 @@
 package com.m.dao;
 
+import java.util.Date;
+import java.util.List;
+import com.p.model.HallOrderBean;
+
 public interface TicketSaleDao {
 
-	//廳院(包廳)座位數DBT:Hall ==>　hallId, noOfSeats
-	public Integer getNumberOfSeats(Integer hallID);
+	//廳院(包廳)座位數DBT:numberOfSeats => hallId, noOfSeats, date
+	//DBT: showTimeHistory => hallID, runID, palyStartTime播放日期時間
+	
+	//取得廳院的ID跟date
+	
+	//取得廳院&包廳座位數
+	public Integer getHallSeats(String hallID, Date date);
 	
 	//DBT: movies => movieID, title, runningTime片長, status上映狀態
 
 	//DBT: running => release上映日, offDate實際下映日
 	
-	//DBT: showTimeHistory => hallID, runID, palyStartTime播放日期時間
-	
-	//包廳DBT:HallOrder => hallNo(名稱不一致), startTime, endTime, hallSubtotal租借總價格, payStatusNo(必須是1才是已付款)
-	
-	//取得已付款包廳價 DBT:HallOrder => hallSubtotal租借總價格, payStatusNo(必須是1才是已付款)
-	public Integer getHallPrice(Integer hallNo);
+	//包廳DBT:hallOrder => hallID, startTime, endTime, hallSubtotal租借總價格, payStatusNo(必須是1才是已付款)
+	public List<HallOrderBean> getHallOrderInfo();
+	//取得已付款包廳價 DBT:hallOrder => hallSubtotal租借總價格, payStatusNo(必須是1才是已付款)
+	public Integer getHallOrderPrice(String hallID);
 }
