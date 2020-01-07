3010ae2e088c6a3812eab15286eafd9dbc2fb6c2
why771102 <why771102@gmail.com>
2020-01-07 20:56:19 +0800

    no message

--- Project3/src/main/java/com/z/model/QuestionContentBean.java
+++ Project3/src/main/java/com/z/model/QuestionContentBean.java
@@ -21,7 +21,7 @@ public class QuestionContentBean implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	@Id
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name="questionId")
 	QuestionBean questionBean;
