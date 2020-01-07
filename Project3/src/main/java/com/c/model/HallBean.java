732c7372ee05b33d6a0c08bab97a9b78283314d2
Ally <deletelin365@gmail.com>
2020-01-07 18:57:55 +0800

    no message

--- Project3/src/main/java/com/c/model/HallBean.java
+++ Project3/src/main/java/com/c/model/HallBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -12,7 +13,7 @@ public class HallBean implements Serializable{
 
 
 	private static final long serialVersionUID = 1L;
-
+   @Id
 	String hallID;
 	String hallName;
 	Integer noOfSeats;
