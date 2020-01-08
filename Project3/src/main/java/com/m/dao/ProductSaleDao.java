30f29feaa9ac52274f927d55b77185e464abce88
Mary <uniwanling@gmail.com>
2020-01-09 01:05:49 +0800

    no message

--- Project3/src/main/java/com/m/dao/ProductSaleDao.java
+++ Project3/src/main/java/com/m/dao/ProductSaleDao.java
@@ -1,13 +1,34 @@
 package com.m.dao;
 
+import java.sql.Timestamp;
+import java.util.List;
+import com.l.model.ProductsBean;
+import com.l.model.mOrderDetailBean;
+
 public interface ProductSaleDao {
 	
-	//DBT:Products => productID, productName, category(4食物 & 5純周邊)
+	//DBT:products => productID, productName, category(4套票(飲食), 5飲食, 6線上商城?)
+	public List<ProductsBean> getProductsInfo(Integer category);
+//	public Integer getProductID(Integer category);
+//	public String getProductName(Integer ProductID);
 	
-	public Integer getProductID(Integer category);
+	//DBT: products & mOrderDetail, products & SCOrderDetail (PID)
+	public List<String> getProductsName(Timestamp orderDate);
+//	public List<String> getPeripheralsName(Timestamp orderDate);
 	
-	public String getProductName(Integer ProductID);
+	//DBT: mOrderDetail => productID, unitPrice, quantity, discount
+	//subtotal: 4+5
+	//subtotal: 6 (DBT: SCOrderDetail(QTY), mOrderDetail(price, discount?), SCOrders(total))
+	public List<mOrderDetailBean> showProductOrder(Integer productID, Timestamp orderDate);
+	//	public mOrderDetailBean showProductOrder1(Integer productID);
 	
-	//DBT: OrderDetails => productID, unitPrice, quantity, discount
-	public List<ProductBean> showProductOrder(Integer productID);
+	//周邊是6開始嗎?
+	//discount是int??
+	
+	//DBT: SCOrderDetail => SCOrderID, productID, quantity
+//	public List<SCOrderDetailBean> getPeripheralsOD(Integer productID);
+	
+	//DBT: SCOrders => orderDate(timestamp), total
+//	public SCOrdersBean getPeripheralsOrder(Integer SCOrderID);
+	
 }
