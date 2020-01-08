12189c2fd1c052ea458ebef6e5895695c459778d
why771102 <why771102@gmail.com>
2020-01-08 14:00:25 +0800

    no message

--- Project3/src/main/java/com/z/dao/impl/EmpDaoImpl.java
+++ Project3/src/main/java/com/z/dao/impl/EmpDaoImpl.java
@@ -29,19 +29,19 @@ public class EmpDaoImpl implements EmpDao {
 
 	@Override
 	public void updateEmp(EmpBean mb) {
-		String hql = "update MemberBean set memberName = :memberName, roleId = :roleId , email = :email, status = :status where memberId = :memberId";
+		String hql = "update EmpBean set empName = :empName, roleId = :roleId , email = :email, status = :status where empId = :empId";
 		Session session = factory.getCurrentSession();
-		session.createQuery(hql).setParameter("memberName", mb.getEmpName())
+		session.createQuery(hql).setParameter("empName", mb.getEmpName())
 								.setParameter("roleId", mb.getRoleId())
 								.setParameter("email", mb.getEmail())
 								.setParameter("status", mb.getStatus())
-								.setParameter("memberId", mb.getEmpId()).executeUpdate();
+								.setParameter("empId", mb.getEmpId()).executeUpdate();
 		
 	}
 
 	@Override
 	public EmpBean isExists(EmpBean mb) {
-		String hql = "from MemberBean where memberId = :memberId";
+		String hql = "from EmpBean where empId = :empId";
 		Session session = factory.getCurrentSession();
 		EmpBean nmb = null;
 		nmb = (EmpBean) session.createQuery(hql).getSingleResult();
@@ -51,7 +51,7 @@ public class EmpDaoImpl implements EmpDao {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<EmpBean> findAllEmps() {
-		String hql = "from memberBean";
+		String hql = "from EmpBean";
 		Session session = factory.getCurrentSession();
 		List<EmpBean> list = new ArrayList<>();
 		list = session.createQuery(hql).getResultList();
