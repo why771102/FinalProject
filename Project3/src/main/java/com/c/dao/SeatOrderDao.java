00dd3f00a3b3c4b92bc251141fc440bc1877017a
Genie <genie.sheu@gms.tku.edu.tw>
2020-01-09 16:57:03 +0800

    no message

--- Project3/src/main/java/com/c/dao/SeatOrderDao.java
+++ Project3/src/main/java/com/c/dao/SeatOrderDao.java
@@ -1,11 +1,12 @@
 package com.c.dao;
 
 import com.a.model.ShowTimeHistoryBean;
+import com.c.model.SeatOrderBean;
 import com.c.model.SeatsBean;
 
 public interface SeatOrderDao {
 
-	public void insertSeatOrder();
+	public void insertSeatOrder(SeatOrderBean sob);
 
 	public SeatsBean getSeatsById(String seatID);
 
