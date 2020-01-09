f08ac175cb1c706f562de2a6eaebeb584607a7ce
luts01 <z2036378@hotmail.com>
2020-01-09 19:27:29 +0800

    no message

--- Project3/src/main/java/com/l/controller/ProductsController.java
+++ Project3/src/main/java/com/l/controller/ProductsController.java
@@ -1,15 +1,23 @@
 package com.l.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.l.model.ProductsBean;
+import com.l.service.ProductsService;
 
 
 @Controller
 public class ProductsController {
+	ProductsService service;
+	@Autowired
+	public void setService(ProductsService service) {
+		this.service = service;
+	}
+		
 	//測試查詢
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	public String getProducts(Model model) {
