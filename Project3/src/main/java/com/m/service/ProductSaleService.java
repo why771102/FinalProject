f467df4a035022b9e5156e4e9299cb901c4ae284
Mary <uniwanling@gmail.com>
2020-01-09 17:22:11 +0800

    no message

--- Project3/src/main/java/com/m/service/ProductSaleService.java
+++ Project3/src/main/java/com/m/service/ProductSaleService.java
@@ -1,22 +1,10 @@
-package com.m.dao;
+package com.m.service;
 
 import java.util.List;
+
 import com.m.model.ProductSaleBean;
 
-public interface ProductSaleDao {
-	
-	//DBT:products => productID, productName, category(4套票(飲食), 5飲食, 6~線上商城)
-//	public List<ProductsBean> getProductsInfo(Integer category, String orderDate);
-//	public Integer getProductID(Integer category);
-//	public String getProductName(Integer ProductID);
-	
-	//DBT: showTimeHistory => showTimeID, playStartTime
-//	public Timestamp getPlayStartTime(Integer showTimeID);
-	
-	//DBT: products & mOrderDetail, products & SCOrderDetail (PID)
-//	public List<String> getProductsName(String orderDate);
-//	public List<String> getPeripheralsName(Timestamp orderDate);
-	
+public interface ProductSaleService {
 	//DBT: mOrderDetail => productID, unitPrice, quantity, discount
 	//show all food orders
 	public List<ProductSaleBean> showAllFoodOrders(String playStartTimeA, String playStartTimeB);
@@ -31,15 +19,6 @@ public interface ProductSaleDao {
 	//6+7+8..
 	public List<ProductSaleBean> showPeriperalOrders(Integer categoryA, Integer categoryB, String orderDateA, String orderDateB);
 	
-//
-//	public List<ProductSaleBean> showDailyFoodOrder(String productName);
 	//根據產品名稱進入單日資訊
 	public List<ProductSaleBean> showFoodOrderByTime(String productName, String playStartTimeA, String playStartTimeB);
-	public List<ProductSaleBean> showPeriperalOrderByTime(String productName, String orderDateA, String orderDateB);
-	//DBT: SCOrderDetail => SCOrderID, productID, quantity
-//	public List<SCOrderDetailBean> getPeripheralsOD(Integer productID);
-	
-	//DBT: SCOrders => orderDate(timestamp), total
-//	public SCOrdersBean getPeripheralsOrder(Integer SCOrderID);
-	
 }
---
+++ Project3/src/main/java/com/m/service/impl/test.java
@@ -0,0 +1,22 @@
+package com.m.service.impl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+
+	public static void main(String[] args) {
+		String playStartTimeA = "2020-01-01";
+		String playStartTimeB = "2020-01-09";
+		LocalDate start = LocalDate.parse(playStartTimeA);
+		LocalDate end = LocalDate.parse(playStartTimeB);
+		List<LocalDate> totalDates = new ArrayList<>();
+		while (!start.isAfter(end)) {
+		    totalDates.add(start);
+		    start = start.plusDays(1);
+		}
+		System.out.println(totalDates);
+	}
+
+}
