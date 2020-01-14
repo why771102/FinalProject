f2a88a83916729c774fe4916bd70d80dc88dac99
luts01 <z2036378@hotmail.com>
2020-01-14 14:53:47 +0800

    no message

--- Project3/src/main/java/com/l/controller/mOrderController.java
+++ Project3/src/main/java/com/l/controller/mOrderController.java
@@ -24,45 +24,45 @@ public class mOrderController {
 		this.service = service;
 	}
 		
-	//測試查詢多筆
-	@RequestMapping("/bookticket")
-	public String getProducts(Model model) {
-		List<MOrderBean> list=service.getProducts();
-		model.addAttribute("Products", list);
-		return "l/bookticket";
-	}
-	//測試查詢類別們
-		@RequestMapping("/queryCategories")
-		public String queryCategories(Model model) {
-			List<String> list=service.getCategories();
-			model.addAttribute("CategoryList", list);
-			return "l/category";
-		}
-		//測試查詢類別用ID 
-			@RequestMapping("/products/{category}")
-				public String queryCategory(@PathVariable("category")Integer category,Model model) {
-					List<ProductsBean> products=service.getCategory(category);
-					model.addAttribute("Products", products);
-					return "l/products";
-				}
-	
-			//測試查詢單筆
-			@RequestMapping("/product")
-			public String getProduct(@RequestParam("id")Integer productID,Model model) {
-				model.addAttribute("Product",service.getProduct(productID));
-				return "l/product";
-			}
-			
-	//測試新增方法*2       
-		@RequestMapping(value = "/products/add", method = RequestMethod.GET)
-		public String getinsertProduct(Model model) {
-			ProductsBean pb = new ProductsBean();
-			model.addAttribute("ProductsBean", pb);
-			return "l/insertproducts";
-	}
-		@RequestMapping(value = "/products/add", method = RequestMethod.POST)
-		public String proccessinsertProduct(@ModelAttribute("ProductsBean") ProductsBean pb) {
-			service.insertProduct(pb);
-			return "redirect:/products";
-		}
+//	//測試查詢多筆
+//	@RequestMapping("/bookticket")
+//	public String getProducts(Model model) {
+//		List<MOrderBean> list=service.getProducts();
+//		model.addAttribute("Products", list);
+//		return "l/bookticket";
+//	}
+//	//測試查詢類別們
+//		@RequestMapping("/queryCategories")
+//		public String queryCategories(Model model) {
+//			List<String> list=service.getCategories();
+//			model.addAttribute("CategoryList", list);
+//			return "l/category";
+//		}
+//		//測試查詢類別用ID 
+//			@RequestMapping("/products/{category}")
+//				public String queryCategory(@PathVariable("category")Integer category,Model model) {
+//					List<ProductsBean> products=service.getCategory(category);
+//					model.addAttribute("Products", products);
+//					return "l/products";
+//				}
+//	
+//			//測試查詢單筆
+//			@RequestMapping("/product")
+//			public String getProduct(@RequestParam("id")Integer productID,Model model) {
+//				model.addAttribute("Product",service.getProduct(productID));
+//				return "l/product";
+//			}
+//			
+//	//測試新增方法*2       
+//		@RequestMapping(value = "/products/add", method = RequestMethod.GET)
+//		public String getinsertProduct(Model model) {
+//			ProductsBean pb = new ProductsBean();
+//			model.addAttribute("ProductsBean", pb);
+//			return "l/insertproducts";
+//	}
+//		@RequestMapping(value = "/products/add", method = RequestMethod.POST)
+//		public String proccessinsertProduct(@ModelAttribute("ProductsBean") ProductsBean pb) {
+//			service.insertProduct(pb);
+//			return "redirect:/products";
+//		}
 }
