409e50a15c1a221c4f3f88e83ed2c71def301912
luts01 <z2036378@hotmail.com>
2020-01-14 14:51:39 +0800

    no message

--- Project3/src/main/java/com/l/dao/mOrderDao.java
+++ Project3/src/main/java/com/l/dao/mOrderDao.java
@@ -2,15 +2,15 @@ package com.l.dao;
 
 import java.util.List;
 
-
+import com.a.model.ShowTimeHistoryBean;
 import com.l.model.MOrderBean;
 import com.l.model.ProductsBean;
 
 public interface mOrderDao {
-	//查詢場地票
-	public List<MOrderBean> getProducts();
-
-		
+	//查詢票
+	public List<MOrderBean> getTickets();
+	//存場地ID需要用到的方法
+	public void getShowTimebyID(ShowTimeHistoryBean ShowTimeID);
 	//更新產品
 	public void updateProducts(ProductsBean product);
 	//新增產品
