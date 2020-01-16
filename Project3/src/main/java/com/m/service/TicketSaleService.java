8139128245c8cefaedb1d1d2b27cb382adde6f01
Mary <uniwanling@gmail.com>
2020-01-16 17:34:36 +0800

    no message

--- Project3/src/main/java/com/m/service/TicketSaleService.java
+++ Project3/src/main/java/com/m/service/TicketSaleService.java
@@ -5,7 +5,6 @@ import com.a.model.RunningBean;
 import com.a.model.ShowTimeHistoryBean;
 import com.c.model.NumberOfSeatsBean;
 import com.l.model.MOrderBean;
-import com.l.model.MOrderDetailBean;
 import com.m.model.TicketSaleBean;
 
 public interface TicketSaleService {
@@ -15,16 +14,16 @@ public interface TicketSaleService {
 	public List<TicketSaleBean> getMOrderDetailBeanList(List<TicketSaleBean> tsbList);
 
 	public List<TicketSaleBean> comparedByTime(String sDate,String eDate); 	//input篩選
-	public List<TicketSaleBean> getHallSaleOutput(List<TicketSaleBean> tsbList); //title篩選
+	public List<TicketSaleBean> getTicketSaleOutput(List<TicketSaleBean> tsbList); //title篩選
 	public List<String> getDistinctTitles();
-//	public List<TicketSaleBean> countShowtime(List<TicketSaleBean> tsbList);
 	public Integer countRepeatedIntegers(List<Integer> ints);
+	public List<TicketSaleBean> getTicketSaleByTitle(List<TicketSaleBean> tsbList, String title); //P2 BY TITLE
+	public List<TicketSaleBean> getTicketSaleByDateOutput(String sDate, String eDate); //P2 BY DATE
 	//=======================================================================
 	public List<RunningBean> ShowMovieByRunTime();
 	public List<RunningBean> CompareByTime(String sDate, String eDate);
 	public List<ShowTimeHistoryBean> getshowTimeHistory(List<RunningBean> RBList);
 	public List<NumberOfSeatsBean> getNumberOfSeats(List<ShowTimeHistoryBean> sthbList);
 	public List<NumberOfSeatsBean> getHallSeats(List<NumberOfSeatsBean> nosbList, String sDate, String eDate);
-	//	public List<MovieBean> ShowMovieBean(List<RunningBean> RBListToPage);
 
 }
