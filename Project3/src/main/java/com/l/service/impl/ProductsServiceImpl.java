7f5a21254023c51cf49064341a7cf4a10e24d728
luts01 <z2036378@hotmail.com>
2020-01-18 14:31:18 +0800

    修正categoryID

--- Project3/src/main/java/com/l/service/impl/ProductsServiceImpl.java
+++ Project3/src/main/java/com/l/service/impl/ProductsServiceImpl.java
@@ -43,8 +43,8 @@ public class ProductsServiceImpl implements ProductsService{
 	//用ID查詢分類產品 
 	@Transactional
 	@Override
-	public List<ProductsBean> getCategory(Integer category) {
-		return dao.getCategory(category);
+	public List<ProductsBean> getCategory(Integer categoryID) {
+		return dao.getCategory(categoryID);
 	}
 	
 	//修改
