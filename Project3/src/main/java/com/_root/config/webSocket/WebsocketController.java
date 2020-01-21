5436b7b08c69b6fdfd44d982f42d2aa36e6f08a2
why771102 <why771102@gmail.com>
2020-01-21 19:51:41 +0800

    no message

--- Project3/src/main/java/com/_root/config/webSocket/WebsocketController.java
+++ Project3/src/main/java/com/_root/config/webSocket/WebsocketController.java
@@ -1,5 +1,20 @@
 package com._root.config.webSocket;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
 public class WebsocketController {
+	
+	@MessageMapping("/sendMessage")
+	@SendTo("/topic/message")
+	public Chat sendMessage(Chat message) throws Exception {
+		System.out.println("sendMessage...ConversationMessage=" + message);
+		System.out.println("sendMessage...message.getName()=" + HtmlUtils.htmlEscape(message.getName()));
+		System.out.println("sendMessage...message.getMessage()=" + HtmlUtils.htmlEscape(message.getContent()));
+		return new Chat(HtmlUtils.htmlEscape(message.getName()), HtmlUtils.htmlEscape(message.getContent()));
+	}
 
 }
