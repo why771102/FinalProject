f8a50b968317946ef6d798d12eafe871acf6d446
luts01 <z2036378@hotmail.com>
2020-01-21 09:45:32 +0800

    no message

--- Project3/src/main/java/com/l/controller/ProductsController.java
+++ Project3/src/main/java/com/l/controller/ProductsController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.l.model.CategoriesBean;
 import com.l.model.ProductsBean;
 import com.l.service.ProductsService;
 
