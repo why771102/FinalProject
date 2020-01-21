f38d812002aaa38bed908fb974d89b6c57a1ac89
why771102 <why771102@gmail.com>
2020-01-21 16:33:34 +0800

    no message

--- Project3/src/main/java/com/_root/config/webSocket/WebSocketConfig.java
+++ Project3/src/main/java/com/_root/config/webSocket/WebSocketConfig.java
@@ -1,23 +1,25 @@
 package com._root.config.webSocket;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//@Configuration
-//@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketConfigurer {
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-//	@Autowired
-	private MyHandler myWebSocketHandler;
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
 	
 	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		
-		registry.addHandler(myWebSocketHandler, "/myHandler").addInterceptors();
+		registry.addEndpoint("/myHandler").withSockJS();
 		
 	}
 }
