e0b98a2a91042ff06981f0b4de4961e543fc6cff
why771102 <why771102@gmail.com>
2020-01-28 13:13:29 +0800

    no message

--- Project3/src/main/java/com/_root/config/webSocket/Chat.java
+++ Project3/src/main/java/com/_root/config/webSocket/Chat.java
@@ -6,36 +6,44 @@ import java.time.format.DateTimeFormatter;
 import javax.persistence.Column;
 
 public class Chat {
-	
+
 	private String name;
 	private String content;
 //	@Column(columnDefinition = "smalldatetime")
 	private String endTime;
-	
-	
 
 	public Chat(String name, String content) {
-	super();
-	this.name = name;
-	this.content = content;
-	DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	LocalDateTime time = LocalDateTime.now();
-	String localTime = df.format(time);
-	this.endTime = localTime;
-}
+		super();
+		this.name = name;
+		this.content = content;
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime time = LocalDateTime.now();
+		String localTime = df.format(time);
+		this.setEndTime(localTime);
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
-	
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
 
 }
--- Project3/src/main/java/com/_root/config/webSocket/WebsocketController.java
+++ Project3/src/main/java/com/_root/config/webSocket/WebsocketController.java
@@ -1,21 +1,32 @@
 package com._root.config.webSocket;
 
+import org.json.JSONObject;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+
+import com.google.gson.Gson;
 
 @Controller
 public class WebsocketController {
 	
 	@MessageMapping("/sendMessage")
 	@SendTo("/topic/message")
-	public Chat sendMessage(Chat message) throws Exception {
+	public Chat sendMessage(String message) throws Exception {
 		System.out.println("123");
 		System.out.println("sendMessage...ConversationMessage=" + message);
-		System.out.println("sendMessage...message.getName()=" + HtmlUtils.htmlEscape(message.getName()));
-		System.out.println("sendMessage...message.getMessage()=" + HtmlUtils.htmlEscape(message.getContent()));
-		return new Chat(HtmlUtils.htmlEscape(message.getName()), HtmlUtils.htmlEscape(message.getContent()));
+		JSONObject jsonObj = new JSONObject(message);
+		System.out.println(jsonObj);
+		String name = jsonObj.getString("name");
+		String content = jsonObj.getString("message");
+		System.out.println(name);
+		System.out.println(content);
+		Chat chat = new Chat(name, content);
+//		System.out.println("sendMessage...message.getName()=" + HtmlUtils.htmlEscape(message.getName()));
+//		System.out.println("sendMessage...message.getMessage()=" + HtmlUtils.htmlEscape(message.getContent()));
+//		return new Chat(HtmlUtils.htmlEscape(message.getName()), HtmlUtils.htmlEscape(message.getContent()));
+		return chat;
 	}
 
 }
