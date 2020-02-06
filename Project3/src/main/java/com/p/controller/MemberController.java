f2e38d073d5a1d824f20d2b2e7a613aaac5f13b1
why771102 <why771102@gmail.com>
2020-02-06 15:58:23 +0800

    no message

--- Project3/src/main/java/com/p/controller/MemberController.java
+++ Project3/src/main/java/com/p/controller/MemberController.java
@@ -187,7 +187,7 @@ public class MemberController {
 			model.addAttribute("errorMsgMap", errorMsgMap);
 			return "login";
 		}
-		return "a/movieTheatreIndex"; //到時候要導到LoginSucess頁面
+		return "redirect:/movieIndex"; //到時候要導到LoginSucess頁面
 	}
 	
 	//以下為登出方法
@@ -202,7 +202,7 @@ public class MemberController {
             cookie.setPath("/");
             response.addCookie(cookie);
 		}
-		return "redirect:/";
+		return "redirect:/";                     
 	}
 	
 	
